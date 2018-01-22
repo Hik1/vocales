a35ea22ff197ea1b05873ff9f85ed7eeedae092f
ism49952491 <ism49952491@a09.informatica.escoladeltreball.org>
2018-01-22 13:51:14 +0100

    First upload

---
+++ Hola.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+count=500
+while (count<1000):
+	print "El sueldo es:", count
+        count= count+100 
+
+anio=2001
+while anio<=2018:
+	print "Informes del Año", str(anio)
+	anio+=1
+
---
+++ vocales.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+vocal=raw_input ("Introduce una letra")
+if vocal="a"or vocal="e" or vocal="i"
+print "Es vocal"
+else:
+	print "Es consonante"
